12a5fb230477433343cc56a47811d55cace96b68
Nada Amin <namin@seas.harvard.edu>
2020-11-13 22:44:52 -0500

    dockerize

--- echo_kernel/kernel.py
+++ echo_kernel/kernel.py
@@ -1,3 +1,65 @@
+import docker
+
+import hashlib
+import os
+
+base_image = 'namin/io.livecode.ch'
+user = 'webyrd'
+repo = 'webmk'
+app_image = base_image+'/'+user+'/'+repo
+app_git_url = 'https://github.com/%s/%s.git' % (user,repo)
+
+def init_app_image(img, git_url):
+    c = docker.from_env()
+    m = c.containers.create(base_image, 'git clone --recursive "%s" /home/runner/code' % git_url, user='runner')
+    m.start()
+    s = m.wait()
+    if s['Error']:
+        return {'status':s['StatusCode'], 'out':'error cloning repository %s -- %s' % (git_url,s['Error'])}
+    m.commit(img)
+    return dkr_run(img, 'livecode-install', img, c=c)
+
+def dkr_run(img, cmd, commit=None, timeout=10, c=None):
+    c = c or docker.from_env()
+    r = ""
+    m = c.containers.create(img,
+                            "timeout %d %s" % (timeout, cmd),
+                            user='runner',
+                            environment={'HOME':'/home/runner'},
+                            volumes={'/tmp/snippets': {'bind': '/mnt/snippets', 'mode': 'ro'}},
+                            network_disabled=False)
+    m.start()
+    s = m.wait()['StatusCode']
+    if s!=0:
+        r += "error: (%d)\n" % s
+    if s==137:
+        r += "killed!"
+    elif s==125:
+        r += "timeout!"
+    elif s==124:
+        r += "inifinite loop!"
+    else:
+        r += m.logs().decode('utf-8')
+    if commit:
+        m.commit(commit)
+    return {'status':s, 'out':r}
+
+def snippet_cache(txt):
+    key = hashlib.md5(txt.encode('utf-8')).hexdigest()
+    fn = os.path.join('/tmp/snippets', key)
+    if not os.path.isfile(fn):
+        with open(fn, 'w') as f:
+            f.write(txt)
+    return key
+
+def run(input_main,input_pre='',input_post=''):
+    img = app_image
+    key_main = snippet_cache(input_main)
+    key_pre = snippet_cache(input_pre)
+    key_post = snippet_cache(input_post)
+    o_run = dkr_run(img, 'livecode-run %s %s %s' % (key_main, key_pre, key_post))
+    return o_run['out'], o_run['status']
+
 from ipykernel.kernelbase import Kernel
 
 class EchoKernel(Kernel):
@@ -12,10 +74,16 @@ class EchoKernel(Kernel):
     }
     banner = "Echo kernel - as useful as a parrot"
 
-    def do_execute(self, code, silent, store_history=True, user_expressions=None,
-                   allow_stdin=False):
+    def __init__(self, **kwargs):
+        Kernel.__init__(self, **kwargs)
+        self.pre = ''
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
         if not silent:
-            stream_content = {'name': 'stdout', 'text': code}
+            output, status = run(code, input_pre=self.pre)
+            if status==0:
+                self.pre += '\n' + code
+            stream_content = {'name': 'stdout', 'text': output}
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
         return {'status': 'ok',
